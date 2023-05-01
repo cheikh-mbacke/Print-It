14f5433303534a829b268866b300dcce61a1c90f
Cheikh Mbacké <mbackecheikhouna7@gmail.com>
2023-05-01 15:46:56 +0200

    Mise en place du défilement infini sur le carrousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,67 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrows = document.querySelectorAll('.arrow');
+
+arrows.forEach(arrow => arrow.addEventListener('click', () => {
+	
+	const dotSelected = document.querySelector('.dot_selected')
+	dotSelected.classList.remove('dot_selected')
+
+	let currentSlide = {}
+
+	if (arrow.classList.contains('arrow_left')) {
+
+		const previousDotElement = dotSelected.previousElementSibling
+		
+		if (previousDotElement === null) {
+			document.querySelector(`.dot[data-index="${slides.length - 1}"]`).classList.add('dot_selected')
+			currentSlide = slides[slides.length - 1]
+		}else{
+			const index = previousDotElement.dataset.index
+			previousDotElement.classList.add('dot_selected')
+			currentSlide = slides[index]
+		}
+	}
+
+	if (arrow.classList.contains('arrow_right')) {
+
+		const nextDotElement = dotSelected.nextElementSibling
+		
+		if (nextDotElement === null) {
+			document.querySelector('.dot[data-index="0"]').classList.add('dot_selected')
+			currentSlide = slides[0]
+		}else{
+			const index = nextDotElement.dataset.index
+			nextDotElement.classList.add('dot_selected')
+			currentSlide = slides[index]
+		}
+		
+	}
+
+	const img = document.querySelector('.banner-img')
+	img.src = `assets/images/slideshow/${currentSlide.image}`
+	img.alt = currentSlide.tagLine
+
+}))
+
+
+slides.forEach((slide, index) => {
+
+	let dotClassList = 'dot';
+
+	if(index == 0){
+		const img = document.querySelector('.banner-img')
+		img.src = `assets/images/slideshow/${slide.image}`
+		img.alt = slide.tagLine
+		dotClassList += ' dot_selected'
+	}
+
+	document.querySelector('.dots').insertAdjacentHTML(
+		'beforeend', 
+		`<div class="${dotClassList}" data-index="${index}"></div>`
+	)
+
+})
+
