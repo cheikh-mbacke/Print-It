bf5c040a56d9b7b1c7b79e58cdc3ddcf7a6734be
Cheikh Mbacké <mbackecheikhouna7@gmail.com>
2023-05-01 15:22:12 +0200

    Modification du slide au clic sur le bouton

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,52 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrows = document.querySelectorAll('.arrow');
+
+arrows.forEach(arrow => arrow.addEventListener('click', () => {
+	
+	const dotSelected = document.querySelector('.dot_selected')
+	dotSelected.classList.remove('dot_selected')
+
+	let currentSlide = {}
+
+	if (arrow.classList.contains('arrow_left')) {
+		const previousDotElement = dotSelected.previousElementSibling
+		previousDotElement.classList.add('dot_selected')
+		const index = previousDotElement.dataset.index
+		currentSlide = slides[index]
+	}
+
+	if (arrow.classList.contains('arrow_right')) {
+		const nextDotElement = dotSelected.nextElementSibling
+		nextDotElement.classList.add('dot_selected')
+		const index = nextDotElement.dataset.index
+		currentSlide = slides[index]
+	}
+
+	const img = document.querySelector('.banner-img')
+	img.src = `assets/images/slideshow/${currentSlide.image}`
+	img.alt = currentSlide.tagLine
+
+}))
+
+
+slides.forEach((slide, index) => {
+
+	let dotClassList = 'dot';
+
+	if(index == 0){
+		const img = document.querySelector('.banner-img')
+		img.src = `assets/images/slideshow/${slide.image}`
+		img.alt = slide.tagLine
+		dotClassList += ' dot_selected'
+	}
+
+	document.querySelector('.dots').insertAdjacentHTML(
+		'beforeend', 
+		`<div class="${dotClassList}" data-index="${index}"></div>`
+	)
+
+})
+
